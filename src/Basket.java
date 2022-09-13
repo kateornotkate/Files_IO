09a70300575b134c17b401323d0769ff29d3a612
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-09-13 18:08:02 +0500

    Java.files + .gitignore

---
+++ src/Basket.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+public class Basket {
+    private static String[] products;
+    private static int[] prices;
+    private final int[] amount;
+
+    // конструктор объекта;
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.amount = new int[products.length];
+    }
+
+    // конструктор объекта для метода loadFromBinFile;
+    public Basket(String[] products, int[] prices, int[] amount) {
+        this.products = products;
+        this.prices = prices;
+        this.amount = amount;
+    }
+
+    public void addToCart(int productNumber, int productCount) {
+        amount[productNumber] += productCount;
+    }
+
+    public void printCart() {
+        int sum = 0;
+        System.out.println("Ваша корзина: ");
+        for (int i = 0; i < amount.length; i++) {
+            if (amount[i] != 0) {
+                System.out.println(products[i] + " " + amount[i] + " шт " + prices[i]
+                        + " руб/шт " + (amount[i] * prices[i]) + " руб в сумме.");
+                sum += amount[i] * prices[i];
+            }
+        }
+        System.out.println("Итого: " + sum + " руб.");
+    }
+
+    //  saveTxt(File textFile) - метод сохранения корзины в текстовый файл;
+    public void saveTxt(File textFile) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(textFile));
+        for (int i = 0; i < products.length; i++) {
+            bw.write(products[i] + " " + amount[i] + " шт " + prices[i]
+                    + " руб/шт " + (amount[i] * prices[i]) + " руб в сумме.\n");
+        }
+        bw.close();
+    }
+
+    //  метод восстановления объекта корзины из текстового файла, в который ранее была она сохранена;
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        int productIndex = 0;
+        int[] amount = new int[products.length];
+        BufferedReader br = new BufferedReader(new FileReader(textFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] lines = line.split(" "); //
+            amount[productIndex] = Integer.parseInt(lines[1]);
+            productIndex++;
+        }
+        br.close();
+        return new Basket(products, prices, amount);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = {"Хлеб", "Молоко", "Сыр"};
+        int[] prices = {50, 100, 250};
+
+        Basket basket = new Basket(products, prices);
+        File basketFile = new File("basket.txt");
+
+        if (basketFile.exists()) {
+            System.out.println("""
+                    Данные прошлой корзины были успешно восстановлены.
+                    Нажмите ENTER, если хотите продолжить покупки.
+                    Если хотите сбросить корзину, введите любое число или символ.""");
+            String input = scanner.nextLine().toLowerCase();
+            if (input.equals("")) {
+                basket = Basket.loadFromTxtFile(basketFile);
+            } else {
+                basket = new Basket(products, prices);
+                System.out.println("Ваша корзина пуста. Добавьте необходимые товары.");
+            }
+        } else {
+            basket = new Basket(products, prices);
+        }
+
+        System.out.println("Список возможных товаров для покупки: ");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт");
+        }
+
+        while (true) {
+            System.out.println("Выберите товар и количество или введите \"end\".");
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                basket.saveTxt(basketFile); // сохранение корзины в текстовый файл;
+                break;
+            }
+            String[] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            basket.addToCart(productNumber, productCount);
+        }
+        basket.printCart();
+    }
+}
